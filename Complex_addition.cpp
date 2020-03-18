e18f56efa21dd349c707980ba5df9451d0f71e12
punitmadia <60518501+punitmadia@users.noreply.github.com>
2020-03-18 10:29:02 +0530

    Add files via upload

---
+++ Complex_addition.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+struct complex
+{
+int x,y;
+};
+complex sum(complex a,complex b)
+{
+complex c;
+c.x=a.x+b.x;
+c.y=a.y+b.y;
+return c;
+}
+
+
+
+
+
+
+
+int main()
+{
+complex a;
+cin>>a.x;
+cin>>a.y;
+complex b={5,6};
+complex c=sum(a,b);
+cout<<c.x<<"+";
+cout<<c.y<<"i";
+
+}
